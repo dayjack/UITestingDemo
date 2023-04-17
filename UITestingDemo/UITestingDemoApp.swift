8a264caf7f56354d71a0d21571386be811e92be7
dayjack <frostjanna12@gmail.com>
2023-04-17 19:58:18 +0900

    Initial Commit

---
+++ UITestingDemo/UITestingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITestingDemoApp.swift
+//  UITestingDemo
+//
+//  Created by ChoiYujin on 2023/04/17.
+//
+
+import SwiftUI
+
+@main
+struct UITestingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
